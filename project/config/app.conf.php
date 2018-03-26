3db085e4c64284031f14ced74684979bf9eeaa2a
arthclement <arthclems@gmail.com>
2018-03-26 17:00:20 +0200

    end of day

---
+++ project/config/app.conf.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    'db' => [
+        'table_user' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'user',
+            'dbuser' => 'root',
+            'dbpass' => null
+        ],
+        'table_articles' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'dbname' => 'articles',
+            'dbuser' => 'root',
+            'dbpass' => null
+        ]
+    ]
+];
--- project/src/php/service/register.php
+++ project/src/php/service/register.php
@@ -1,3 +1,26 @@
 <?php
+require_once __DIR__.'/DBConnector.php';
+$dbConfig = include_once __DIR__.'/../../../config/app.conf.php';
+use DBConnector;
 
+if ($_SERVER['METHOD_REQUEST'] == 'POST') {
+    $username = $_POST['username'] ?? null;
+    $password1 = $_POST['password1'] ?? null;
+    $password2 = $_POST['password2'] ?? null;
+    $userFullName = $_POST['userFullName'] ?? null;
+    
+    if (
+        strlen($username) > 2 &&
+        strlen($userFullName) > 2 &&
+        strlen($password1) > 8 &&
+        $password1 == $password2
+    ) {
+        try {
+            DBConnector::setConfig($dbConfig['table_user']);
+            $connection = DBConnector::getConnection();
+        } catch (PDOException $e) {
+
+        }
+    }
+}
 ?>
